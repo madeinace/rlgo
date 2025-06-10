7db90367c58efacb8d0fa9a611eb7b19e3c76085
Ace <ace.jl.han@gmail.com>
2025-06-10 14:20:08 +0800

    make the toy-text environment run

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
+import gymnasium as gym
 import logging
 
 logger = logging.getLogger()
 
 
+env = gym.make("CliffWalking-v0", render_mode="human")
+
+
 def main():
     print("Hello from rl-cliff-walking!")
+    obs, _ = env.reset()
+    while True:
+        action = env.action_space.sample()
+
+        obs, reward, terminated, truncated, info = env.step(action)
+
+        if terminated:
+            break
+    env.close()
 
 
 if __name__ == "__main__":
